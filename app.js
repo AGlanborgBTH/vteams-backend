fb6af40a696b78fff26fd559214f394f2c68d8f7
kalinchukkristina <89469456+kalinchukkristina@users.noreply.github.com>
2022-12-01 23:00:33 +0100

    added body-parser

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-/*
-    Main file for API
-
-    This file will contain:
-        Server start
-        The main endpoints
-        Socket initiation for scooters
-
-    Express:
-        "/v1"
-            Leading to './routes/v1.js'
-*/
-
-
 require("dotenv").config();
 const ver1 = require("./routes/v1.js");
 const mongoose = require("mongoose");
@@ -20,6 +6,7 @@ const port = process.env.PORT || 3000;
 const cors = require("cors");
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 
 
 mongoose.connect(mongoString);
@@ -34,9 +21,12 @@ database.once("connected", () => {
 });
 
 const Server = express();
+Server.use(bodyParser.json());
+Server.use(bodyParser.urlencoded({ extended: true }));
 Server.use(cors());
 Server.use(express.json());
 Server.use(cookieParser());
+
 
 Server.use("/v1", ver1);
 
