b439e970f4f2a6801038056d8c49e262bd91ff09
TinkzBot <zakaria.ziadi92@gmail.com>
2022-12-17 16:36:40 +0100

    Updated "UpdateOneScooter Function" To Allow Faster Robot PATCH Calls.

--- modules/scooter/patch.js
+++ modules/scooter/patch.js
@@ -1,19 +1,18 @@
 const { Scooters } = require("../../models/Scooters");
 
 //Update Scooter By ID
-async function updateOneScooter(req, res) {
-    try {
-        const id = req.params.id;
-        const updatedData = req.body;
-        const options = { new: true };
-
-        const result = await Scooters.findByIdAndUpdate(id, updatedData, options);
-
-        res.send(result);
-      } catch (error) {
-        res.status(500).json({ message: error.message });
-      }
+async function updateOneScooter(req, res) {;
+  const update = req.body;
+  try {
+    const result = await Scooters.updateMany({ inUse: true }, { $set: update });
+    res.send({ modifiedCount: result.modifiedCount });
+    console.log({ modifiedCount: result.modifiedCount });
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
 }
+
 
 module.exports = {
   updateOneScooter
