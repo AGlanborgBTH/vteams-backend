092753f08e4a119ab2a6d9aeb6b39bb879354a28
kalinchukkristina <89469456+kalinchukkristina@users.noreply.github.com>
2023-01-03 01:18:04 +0100

    delete user

--- routes/v1Routes/user.js
+++ routes/v1Routes/user.js
@@ -11,12 +11,14 @@ const {updateUser} = require ('../../modules/user/patch')
 const {postSignUp} = require ('../../modules/user/signup')
 const {postLogin} = require ('../../modules/user/login')
 const {getOauthLogin} = require ('../../modules/user/auth')
+const {deleteOneUser} = require ('../../modules/user/delete')
 
 
 // Get all Users Route
 router.get("/", (req, res) => {return getAllUsers(req, res)});
 router.get("/oauth-callback", (req, res) => {return getOauthLogin(req, res)});
 router.get("/:id", (req, res) => {return getOneUser(req, res)});
+router.delete("/:id", (req, res) => {return deleteOneUser(req, res)});
 router.patch("/:id", (req, res) => {return updateUser(req, res)});
 router.post("/signup", (req, res) => {return postSignUp(req, res)});
 router.post("/login", (req, res) => {return postLogin(req, res)});
